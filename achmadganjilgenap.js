350fc5766636e6256f928d6110042182ab94fa08
Achmad Tirto Sudiro <118708640+Achmadts@users.noreply.github.com>
2022-11-29 11:37:02 +0700

    Update achmadganjilgenap.js

--- achmadganjilgenap.js
+++ achmadganjilgenap.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 var MasukkanNilai = input.questionInt('Masukkan angka = ');
 
 if(MasukkanNilai & 2) {
-    console.log('Bilangan Ganjil');
-} else {
     console.log('Bilangan Genap');
+} else {
+    console.log('Bilangan Ganjil');
 }
